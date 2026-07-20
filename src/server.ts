ff4730af6346af26fc25efcee091769884454f9c
Vicente Ramos <vicenteprfilho09@gmail.com>
2026-07-20 20:52:21 -0300

    feat: cria estrutura inicial do servidor Express

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.use((_req, res) => {
+  res.status(404).json({
+    message: "Not found!.",
+  });
+});
+
+app.listen(Number(process.env.PORT));
