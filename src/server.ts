e045c8c139a5839df1d0143fe7ee410a058f4308
Vicente Ramos <vicenteprfilho09@gmail.com>
2026-07-22 19:05:04 -0300

    feat: registrar rotas de categoria e manipulador de erros no servidor

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,19 @@
 import express from 'express';
+import errorHandler from './middlewares/errorHandler.ts';
+import CategoriaRouter from './routes/categoria.routes.ts';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/categoria', CategoriaRouter);
 
 app.use((_req, res) => {
 	res.status(404).json({
 		message: 'Not found!.',
 	});
 });
+
+app.use(errorHandler);
 
 app.listen(Number(process.env.PORT));
