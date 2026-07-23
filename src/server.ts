861fbda654a410f94a744ed6e3c2677a26a51bb7
Vicente Ramos <vicenteprfilho09@gmail.com>
2026-07-23 18:30:29 -0300

    feat: registrar rotas de produto no servidor

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import errorHandler from './middlewares/errorHandler.ts';
 import CategoriaRouter from './routes/categoria.routes.ts';
+import ProdutoRouter from './routes/produto.routes.ts';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/categoria', CategoriaRouter);
+
+app.use('/produto', ProdutoRouter);
 
 app.use((_req, res) => {
 	res.status(404).json({
