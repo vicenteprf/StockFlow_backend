68144c08d4659ac27542013214b2c2817b24b594
Vicente Ramos <vicenteprfilho09@gmail.com>
2026-07-23 18:27:50 -0300

    feat: adicionar tipos para produto

--- src/types.ts
+++ src/types.ts
@@ -6,3 +6,27 @@ export type Categoria = {
 export type CreateCategoria = Omit<Categoria, 'id'>;
 
 export type UpdateCategoria = Categoria;
+
+export type Produto = {
+	id: number;
+	nome: string;
+	descricao: string | null;
+	preco: number;
+	quantidade: number;
+	validade: Date | null;
+	categoria: Categoria;
+};
+
+export type CreateProduto = Omit<Produto, 'id' | 'categoria'> & {
+	categoria: number;
+};
+
+export type UpdateProduto = {
+	id: number;
+	nome: string;
+	descricao: string | null;
+	preco: number;
+	quantidade: number;
+	validade: Date | null;
+	categoria: number;
+};
