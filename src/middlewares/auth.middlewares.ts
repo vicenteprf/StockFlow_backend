90a7b16f7b7e6a66784505f64241cec5bc677f8c
Vicente Ramos <vicenteprfilho09@gmail.com>
2026-07-29 16:05:18 -0300

    feat: adicionar middleware de autenticacao com validade de token JWT

---
+++ src/middlewares/auth.middlewares.ts
@@ -0,0 +1,27 @@
+import type { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import authConfig from '../config/auth.ts';
+import { UnauthorizedError } from '../errors/index.ts';
+import type { TokenPayload } from '../types.ts';
+
+export default async function authMiddleware(
+	req: Request,
+	_res: Response,
+	next: NextFunction,
+) {
+	const token = req.headers.authorization?.split(' ')[1];
+
+	if (!token) {
+		return next(new UnauthorizedError('Não autorizado.'));
+	}
+
+	try {
+		const validacao = jwt.verify(token, authConfig.secret) as TokenPayload;
+
+		req.id = validacao.id;
+
+		return next();
+	} catch {
+		return next(new UnauthorizedError('Token inválido.'));
+	}
+}
