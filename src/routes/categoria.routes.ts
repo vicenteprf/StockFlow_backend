9ac684f27852d8be176ffa12da34214f8b8bc59f
Vicente Ramos <vicenteprfilho09@gmail.com>
2026-07-29 18:27:58 -0300

    feat: adicionar validacao zod nas rotas de categoria

--- src/routes/categoria.routes.ts
+++ src/routes/categoria.routes.ts
@@ -1,12 +1,25 @@
 import { Router } from 'express';
 import * as CategoriaController from '../controllers/categoria.controller.ts';
+import validate from '../middlewares/validate.middleware.ts';
+import {
+	createCategoriaSchema,
+	updateCategoriaSchema,
+} from '../schemas/categoria.schema.ts';
 
 const router = Router();
 
 router.get('/', CategoriaController.getAllCategoria);
 router.get('/:id', CategoriaController.getCategoriaById);
-router.post('/', CategoriaController.createCategoria);
-router.put('/:id', CategoriaController.updateCategoria);
+router.post(
+	'/',
+	validate(createCategoriaSchema),
+	CategoriaController.createCategoria,
+);
+router.put(
+	'/:id',
+	validate(updateCategoriaSchema),
+	CategoriaController.updateCategoria,
+);
 router.delete('/:id', CategoriaController.deleteCategoria);
 
 export default router;
