262fbf0f7d921c0cd79f53ac482d1c20942a21ea
Vicente Ramos <vicenteprfilho09@gmail.com>
2026-07-30 18:54:42 -0300

    refactor: mover authMiddleware para rotas especificas de usuario

--- src/routes/usuario.routes.ts
+++ src/routes/usuario.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import * as UsuarioController from '../controllers/usuario.controller.ts';
+import authMiddleware from '../middlewares/auth.middlewares.ts';
 import validate from '../middlewares/validate.middleware.ts';
 import { createUsuarioSchema } from '../schemas/usuario.schema.ts';
 
 const router = Router();
 
-router.get('/', UsuarioController.getAllUsuario);
-router.get('/:id', UsuarioController.getUsuarioById);
+router.get('/', authMiddleware, UsuarioController.getAllUsuario);
+router.get('/:id', authMiddleware, UsuarioController.getUsuarioById);
 router.post(
 	'/',
 	validate(createUsuarioSchema),
