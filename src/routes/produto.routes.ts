f1f416f5aa9ba4f30b629ee65b6b27b283fc1615
Vicente Ramos <vicenteprfilho09@gmail.com>
2026-07-29 21:10:32 -0300

    feat: adicionar validacao zod nas rotas de produto e usuario

--- src/routes/produto.routes.ts
+++ src/routes/produto.routes.ts
@@ -1,12 +1,25 @@
 import { Router } from 'express';
 import * as ProdutoController from '../controllers/produto.controller.ts';
+import validate from '../middlewares/validate.middleware.ts';
+import {
+	createProdutoSchema,
+	updateProdutoSchema,
+} from '../schemas/produto.schema.ts';
 
 const router = Router();
 
 router.get('/', ProdutoController.getAllProduto);
 router.get('/:id', ProdutoController.getProdutoById);
-router.post('/', ProdutoController.createProduto);
-router.put('/:id', ProdutoController.updadeProduto);
+router.post(
+	'/',
+	validate(createProdutoSchema),
+	ProdutoController.createProduto,
+);
+router.put(
+	'/:id',
+	validate(updateProdutoSchema),
+	ProdutoController.updadeProduto,
+);
 router.delete('/:id', ProdutoController.deleteProduto);
 
 export default router;
--- src/routes/usuario.routes.ts
+++ src/routes/usuario.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import * as UsuarioController from '../controllers/usuario.controller.ts';
+import validate from '../middlewares/validate.middleware.ts';
+import { createUsuarioSchema } from '../schemas/usuario.schema.ts';
 
 const router = Router();
 
 router.get('/', UsuarioController.getAllUsuario);
 router.get('/:id', UsuarioController.getUsuarioById);
-router.post('/', UsuarioController.createUsuario);
+router.post(
+	'/',
+	validate(createUsuarioSchema),
+	UsuarioController.createUsuario,
+);
 
 export default router;
