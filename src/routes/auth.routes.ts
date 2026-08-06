94dc1a1efcf1ef847fbbc916c33d3b85abcf15ff
Vicente Ramos <vicenteprfilho09@gmail.com>
2026-08-06 20:01:07 -0300

    feat: adicionar rotas de autenticacao google

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
-import * as AuthController from '../controllers/login.controller.ts';
+import * as AuthController from '../controllers/auth.controller.ts';
 
 const router = Router();
 
 router.post('/', AuthController.login);
+
+router.get('/google', AuthController.googleRedirect);
+router.get('/google/callback', AuthController.googleCallback);
 
 export default router;
